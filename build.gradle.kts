a41e1ba29b49df35e5706369b413dff446a24da0
jocelyn.ntakpe <jocelyn.ntakpe@manomano.com>
2022-04-28 21:45:19 +0200

    build: configure gradle to support both Github and Maven central artifact deployment. Maven central requires signing and documenting artifacts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,15 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.20"
+    kotlin("jvm") version "1.6.21"
+    id("org.jetbrains.dokka") version "1.6.21"
+    `java-library`
+    `maven-publish`
+    signing
 }
 
 group = "org.kmolecules"
-version = "1.0-SNAPSHOT"
+version = "1.0.0-RC1"
 
 repositories {
     mavenCentral()
@@ -21,10 +25,87 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    dokkaJavadoc.configure {
+        outputDirectory.set(buildDir.resolve("javadoc"))
+    }
+    javadoc {
+        dependsOn(dokkaJavadoc)
+        setDestinationDir(buildDir.resolve("javadoc"))
+    }
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components.findByName("java"))
+            pom {
+                name.set(project.name)
+                description.set("jMolecules types for Kotlin")
+                url.set("https://github.com/jntakpe/kmolecules")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("jntakpe")
+                        name.set("Jocelyn NTAKPE")
+                    }
+                }
+                issueManagement {
+                    system.set("Github issues")
+                    url.set("https://github.com/jntakpe/kmolecules/issues")
+                }
+                ciManagement {
+                    system.set("Github actions")
+                    url.set("https://github.com/jntakpe/kmolecules/actions")
+                }
+                scm {
+                    connection.set("scm:git:git@github.com:jntakpe/kmolecules.git")
+                    developerConnection.set("scm:git:git@github.com:jntakpe/kmolecules.git")
+                    url.set("https://github.com/jntakpe/kmolecules/")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "Maven_central"
+            setUrl("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+            credentials {
+                val sonatypeUsername: String? by project
+                val sonatypePassword: String? by project
+                username = sonatypeUsername
+                password = sonatypePassword
+            }
+        }
+        maven {
+            name = "Github_packages"
+            setUrl("https://maven.pkg.github.com/jntakpe/kmolecules")
+            credentials {
+                val githubActor: String? by project
+                val githubToken: String? by project
+                username = githubActor
+                password = githubToken
+            }
+        }
+    }
+}
+
+signing {
+    if ("CI" in System.getenv()) {
+        val signingKey: String? by project
+        val signingPassword: String? by project
+        useInMemoryPgpKeys(signingKey, signingPassword)
+    }
+    sign(publishing.publications["mavenJava"])
 }
